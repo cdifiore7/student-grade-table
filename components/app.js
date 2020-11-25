a3280c54683b22846d9a0e83384f01a6df610429
Chris Difiore <christopher.difiore@gmail.com>
2020-11-24 19:32:29 -0800

    Completed View All Grades

--- components/app.js
+++ components/app.js
@@ -1,27 +1,28 @@
+/* eslint-disable no-undef */
 class App {
+  constructor(gradeTable) {
+    this.gradeTable = gradeTable
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this)
+  }
   handleGetGradesError(error) {
     console.log(error);
   }
   handleGetGradesSuccess(grades) {
-    this.gradeTable.updatesGrades(grades);
-  }
-  constructor(gradeTable) {
-    this.handleGetGradesError = this.handleGetGradesError(this);
-    this.handleGetGradesSuccess = this.handleGetGradesSuccess(this)
-    this.gradeTable = this.gradeTable
+    this.gradeTable.updateGrades(grades);
   }
   getGrades() {
-    var ajax = {
-      method: "GET",
+    var load = {
+      method: 'GET',
+      url: 'https://sgt.lfzprototypes.com/api/grades',
       success: this.handleGetGradesSuccess,
       error: this.handleGetGradesError,
       headers: {
-        "X-Access-Token": ""
-      },
-    }
+        "X-Access-Token" : "8xQfzCzF"
+    }}
+$.ajax(load)
   }
-  $.ajax("https://sgt.lfzprototypes.com/api/grades", ajax)
-  start() {
-    this.getGrades()
+  start(){
+    this.getGrades();
   }
 }
--- components/grade-table.js
+++ components/grade-table.js
@@ -3,8 +3,8 @@ class GradeTable {
     this.tableElement = tableElement
   }
   updateGrades(grades) {
-    var tbody = this.tableElement.querySelector("tbody");
-    tbody.textContent = ""
+    var tbody = document.querySelector('tbody');
+    tbody.textContent = ''
     for (var i = 0; i < grades.length; i++) {
       var tr = document.createElement("tr")
       var tdname = document.createElement("td")
