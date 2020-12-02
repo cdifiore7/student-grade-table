b05bd00178c501352310b0645952da9524eb2313
Chris Difiore <christopher.difiore@gmail.com>
2020-12-01 22:13:39 -0800

    Completed

--- components/app.js
+++ components/app.js
@@ -1,16 +1,24 @@
-/* eslint-disable no-undef */
 class App {
-  constructor(gradeTable) {
+  constructor(gradeTable, pageHeader) {
     this.gradeTable = gradeTable
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this)
+    this.pageHeader = pageHeader;
   }
   handleGetGradesError(error) {
     console.log(error);
   }
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades);
+    var total= 0;
+    for (var i=0; i < grades.length; i++) {
+      total += grades[i].grade
+    }
+    var avg = total / grades.length;
+    this.pageHeader.updateAverage(avg);
+
   }
+
   getGrades() {
     var load = {
       method: 'GET',
