5dbf8fa47f13868daa541d5f117e21d7687eae9b
Chris Difiore <christopher.difiore@gmail.com>
2020-12-04 15:20:47 -0800

    Gradeform

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,9 +1,14 @@
 class GradeForm {
   constructor(formElement) {
-    this.formElement = formElement
+    this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener("submit", this.handleSubmit)
   }
   onSubmit(createGrade) {
-    this.createGrade = createGrade
+    this.createGrade = createGrade;
   }
-  handleSubmit
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log("handleSubmit called");
+  }
 }
--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-undef */
 var table = document.getElementsByTagName('table');
-var gradeTable = new GradeTable(table)
+var gradeTable = new GradeTable(table);
 var header = document.querySelector('header');
-var pageHead = new PageHeader(header)
-var grade = new App(gradeTable, pageHead)
+var pageHead = new PageHeader(header);
+var formElement= document.querySelector('form')
+var gradeForm = new GradeForm(formElement);
+var grade = new App(gradeTable, pageHead, gradeForm);
 
 grade.start()
