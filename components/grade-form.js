da99474313ee857706a8fee5e1a9233058b8e796
Chris Difiore <christopher.difiore@gmail.com>
2020-12-05 11:41:56 -0800

    Completed Branch

--- components/grade-form.js
+++ components/grade-form.js
@@ -2,13 +2,18 @@ class GradeForm {
   constructor(formElement) {
     this.formElement = formElement;
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.formElement.addEventListener("submit", this.handleSubmit)
+    this.formElement.addEventListener("submit", this.handleSubmit);
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade;
   }
   handleSubmit(event) {
-    event.preventDefault()
-    console.log("handleSubmit called");
+    event.preventDefault();
+    var formdata = new FormData(event.target);
+    var studentname = formdata.get('name');
+    var studentcourse = formdata.get('course');
+    var studentgrade = formdata.get('grade');
+    this.createGrade(studentname, studentcourse, studentgrade);
+    event.target.reset();
   }
 }
--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-/* eslint-disable no-undef */
-var table = document.getElementsByTagName('table');
+var table = document.querySelector('table');
 var gradeTable = new GradeTable(table);
 var header = document.querySelector('header');
 var pageHead = new PageHeader(header);
-var formElement= document.querySelector('form')
+var formElement= document.querySelector('form');
 var gradeForm = new GradeForm(formElement);
-var grade = new App(gradeTable, pageHead, gradeForm);
+var app = new App(gradeTable, pageHead, gradeForm);
 
-grade.start()
+app.start();
