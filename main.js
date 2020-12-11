8e3a936dd85f5367f2ad5a07ea250deacfc6d0b3
Chris Difiore <christopher.difiore@gmail.com>
2020-12-10 21:12:57 -0800

    Completed Branch

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var table = document.querySelector('table');
-var gradeTable = new GradeTable(table);
+var gradeTable = new GradeTable(table, p);
 var header = document.querySelector('header');
 var pageHead = new PageHeader(header);
 var formElement= document.querySelector('form');
 var gradeForm = new GradeForm(formElement);
 var app = new App(gradeTable, pageHead, gradeForm);
-
+var p = document.querySelector('p');
 app.start();
